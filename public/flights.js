dba6985861752c6c471d7ddecea05c2aa02e230a
manueljenni <manuelnoahjenni@gmail.com>
2022-07-16 00:16:59 +0200

    Update modal

--- public/flights.js
+++ public/flights.js
@@ -35,19 +35,11 @@ function UpcomingFlightsTable(props) {
   const [error, setError] = useState(null);
   const { height, width } = useWindowDimensions();
 
-  var tripId = props.tripId;
-
-  if (tripId == undefined) {
-    tripId = '';
-  } else {
-    tripId = '/?tripId=' + props.tripId;
-  }
-
   useEffect(() => {
       const getData = async () => {
         try {
           const response = await fetch(
-              'https://damp-atoll-27311.herokuapp.com/api/flights/getAllUpcomingFlights' + tripId
+              'https://damp-atoll-27311.herokuapp.com/api/flights/getAllUpcomingFlights'
           );
           if (!response.ok) {
             throw new Error(
@@ -234,5 +226,98 @@ function parseDate(input_date) {
 
   return monthNames[date.getMonth()] + ' ' + date.getDate() + ', ' + date.getFullYear();
 }
+
+
+function FlightsByTripTable(props) {
+
+  const [article, setArticle] = useState();
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+  const { height, width } = useWindowDimensions();
+
+  var tripId = props.tripId;
+
+  useEffect(() => {
+      const getData = async () => {
+        try {
+          const response = await fetch(
+              'https://damp-atoll-27311.herokuapp.com/api/flights/getAllUpcomingFlights' + tripId
+          );
+          if (!response.ok) {
+            throw new Error(
+              `This is an HTTP error: The status is ${response.status}`
+            );
+          }
+          let actualData = await response.json();
+          setData(actualData);
+          console.log(actualData);
+          setError(null);
+        } catch(err) {
+          setError(err.message);
+          setData(null);
+        } finally {
+          setLoading(false);
+        }  
+      }
+      getData()
+
+    }, [])
+
+    if (width < 1000) {
+      return (
+        <div className="table w-full border-collapse border" key="table1">
+          <div className="table-header-group font-bold">
+            <div className="table-row bg-accentColor">
+              <div className="table-cell text-xl px-4 py-4 w-1/2">Origin</div>
+              <div className="table-cell text-xl px-4 py-4 w-1/2">Destination</div>
+            </div>
+          </div>
+          <div className="table-row-group">
+          {data &&
+              data.map(flight => (
+              <div key={flight.departureTime + "_" + flight.departure_iata + flight.arrival_iata} className="table-row even:bg-gray-200 hover:bg-accentColorHover hover:cursor-pointer" onClick={() => fitBounds([flight.departure.longitude, flight.departure.latitude], [flight.arrival.longitude, flight.arrival.latitude])}>
+                <div className="table-cell text-xl px-4 py-4"><span className="text-gray-400">{parseDate(flight.departureTime)}</span><br/><b>{flight.departure.city}</b><br/>({flight.departure.iata})</div>
+                <div className="table-cell text-xl px-4 py-4"><span className="text-gray-400">{parseDate(flight.arrivalTime)}</span><br/><b>{flight.arrival.city}</b><br/>({flight.arrival.iata})</div>
+              </div>
+              ))}
+          </div>
+        </div>
+      );
+    } else {
+        return (
+          <div className="table w-full border-collapse border" key="table1">
+            <div className="table-header-group font-bold">
+              <div className="table-row bg-accentColor">
+                <div className="table-cell text-xl px-4 py-4">Date</div>
+                <div className="table-cell text-xl px-4 py-4">Origin</div>
+                <div className="table-cell text-xl px-4 py-4">Destination</div>
+                <div className="table-cell text-xl px-4 py-4">Airline</div>
+                <div className="table-cell text-xl px-4 py-4">Distance</div>
+                <div className="table-cell text-xl px-4 py-4">Duration</div>
+              </div>
+            </div>
+            <div className="table-row-group">
+            {data &&
+              data.map(flight => (
+              <div key={flight.departureTime + "_" + flight.departure_iata + flight.arrival_iata} className="table-row even:bg-gray-200 hover:bg-accentColorHover hover:cursor-pointer" onClick={() => fitBounds([flight.departure.longitude, flight.departure.latitude], [flight.arrival.longitude, flight.arrival.latitude])}>
+                <div className="table-cell text-xl px-4 py-4 whitespace-nowrap">{parseDate(flight.departureTime)}</div>
+                <div className="table-cell text-xl px-4 py-4"><b>{flight.departure.city}</b> ({flight.departure.iata})<br/><p className="text-lg text-gray-400">{flight.departure.countryName}</p></div>
+                <div className="table-cell text-xl px-4 py-4"><b>{flight.arrival.city}</b> ({flight.arrival.iata})<br/><p className="text-lg text-gray-400">{flight.arrival.countryName}</p></div>
+                <div className="table-cell text-xl px-4 py-4">{flight.airline.name}<br/><p className="text-lg text-gray-400">{flight.airline.code} {flight.flightNumber}</p></div>
+                <div className="table-cell text-xl px-4 py-4 whitespace-nowrap">{Math.round((flight.miles * 1.852))} km<br/><p className="text-lg text-gray-400">{flight.miles} nmi</p></div>
+                <div className="table-cell text-xl px-4 py-4 whitespace-nowrap">{Math.round((flight.duration / 3600) * 100) / 100} h</div>
+              </div>
+              ))}
+            </div>
+          </div>
+        );
+    }
+    
+}
+
+const domContainer3 = document.querySelector('#flightsByTripTable');
+const root3 = ReactDOM.createRoot(domContainer);
+root3.render(<FlightsByTripTable />);
 
 
--- public/trips_new.js
+++ public/trips_new.js
@@ -59,28 +59,35 @@ function ReactModal(props) {
     }
   })
 
+  var separator = '';
+
+  if (width > 1000) {
+    separator = <span className="text-3xl">&#183;</span>;
+  }
+
   return (
     <div>
       {openModal &&
       <div className="modal rounded-lg" id="modal">
         <div className="modal-content">
-          <div className="px-8 flex justify-between">
+          <div className="px-8 pt-4 flex justify-between">
             <div>
-              <div className="sd:flex-col lg:flex space-x-4 mb-2">
-                <h1 className="text-3xl font-semibold">Asia Summer 2022</h1>              
+              <div className="sd:flex-col lg:flex lg:space-x-4 mb-2">
+                <h1 className="text-3xl font-semibold">Asia Summer 2022</h1>  
+                {separator}
                 <h1 className="text-3xl text-neutral-800">Aug 2022</h1>
               </div>
             </div>
-            <button className="button m-12 closeModal">Close</button>
+            <button className="button closeModal h-10 pl-4">Close</button>
           </div>
-          <div className="px-8">
-            <p className="text-2xl text-neutral-600">Singapore, Thailand, Indonesia</p>
+          <div className="px-8 pb-8">
+            <p className="text-2xl text-neutral-600 mb-6">Singapore, Thailand, Indonesia</p>
             <p className="text-lg"><b>Dates:</b> Aug 04, 2022 - Aug 26, 2022</p>
             <p className="text-lg"><b>Duration:</b> 26 days</p>
           </div>
-          <div className="px-8">
-            <h1 className="text-3xl text-neutral-800 font-semibold">Flights</h1>
-            <UpcomingFlightsTable />
+          <div className="px-8 pb-4">
+            <h1 className="text-3xl text-neutral-800 font-semibold pb-6">Flights</h1>
+            <FlightsByTripTable />
           </div>
         </div> 
       </div>
