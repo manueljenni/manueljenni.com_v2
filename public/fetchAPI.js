4dbf3ee54b66c857da512e1beb5ab04b98c1486d
Jenni Manuel <5702@intranet.trisa>
2022-11-16 13:49:17 +0100

    Fix code style

--- public/fetchAPI.js
+++ public/fetchAPI.js
@@ -12,56 +12,56 @@ function FeaturedArticles(props) {
   const [error, setError] = useState(null);
 
   useEffect(() => {
-      const getData = async () => {
-        try {
-          const response = await fetch(
-              'https://damp-atoll-27311.herokuapp.com/api/articles/getArticleSummaryByCategory?category=featured'
+    const getData = async () => {
+      try {
+        const response = await fetch(
+          'https://damp-atoll-27311.herokuapp.com/api/articles/getArticleSummaryByCategory?category=featured'
+        );
+        if (!response.ok) {
+          throw new Error(
+            `This is an HTTP error: The status is ${response.status}`
           );
-          if (!response.ok) {
-            throw new Error(
-              `This is an HTTP error: The status is ${response.status}`
-            );
-          }
-          let actualData = await response.json();
-          setData(actualData);
-          console.log(actualData)
-          setError(null);
-        } catch(err) {
-          setError(err.message);
-          setData(null);
-        } finally {
-          setLoading(false);
-        }  
+        }
+        let actualData = await response.json();
+        setData(actualData);
+        console.log(actualData)
+        setError(null);
+      } catch (err) {
+        setError(err.message);
+        setData(null);
+      } finally {
+        setLoading(false);
       }
-      getData()
+    }
+    getData()
 
-    }, [])
+  }, [])
 
-    return (
-      
-      <div className="space-y-7">
-           {loading && 
-              <div className="space-y-7">
-                  <div className="bg-white rounded-lg flex h-44 drop-shadow-md cursor-pointer hover"></div>
-                  <div className="bg-white rounded-lg flex h-44 drop-shadow-md cursor-pointer hover"></div>
-                  <div className="bg-white rounded-lg flex h-44 drop-shadow-md cursor-pointer hover"></div>
+  return (
+
+    <div className="space-y-7">
+      {loading &&
+        <div className="space-y-7">
+          <div className="bg-white rounded-lg flex h-44 drop-shadow-md cursor-pointer hover"></div>
+          <div className="bg-white rounded-lg flex h-44 drop-shadow-md cursor-pointer hover"></div>
+          <div className="bg-white rounded-lg flex h-44 drop-shadow-md cursor-pointer hover"></div>
+        </div>
+      }
+      {data &&
+        data.reverse().map(({ image, title, summary, category, link }) => (
+          <div key={link} className="bg-white rounded-lg flex drop-shadow-md cursor-pointer hover h-fit max-h-72" onClick={() => window.location.href = ('articles/' + link)}>
+            <img className="hidden md:block md:w-2/5 object-cover rounded-l-lg" src={"img/articles/" + image} />
+            <div className="w-full md:w-3/5 md:flex items-center">
+              <div className="space-y-1 md:space-y-3 px-6 py-4">
+                <p className="text-sm text-highlightColor-400 uppercase">{category}</p>
+                <p className="text-2xl md:text-2.5xl font-semibold font-medium text-neutral-800 leading-8 md:line-clamp-2 hyphens-auto">{title}</p>
+                <p className="text-base leading-5 line-clamp-4 md:line-clamp-3">{summary}</p>
               </div>
-           }
-          {data &&
-          data.reverse().map(({image, title, summary, category, link}) => (
-              <div key={link} className="bg-white rounded-lg flex drop-shadow-md cursor-pointer hover h-fit max-h-72" onClick={() => window.location.href= ('articles/' + link)}>
-              <img className="hidden md:block md:w-2/5 object-cover rounded-l-lg" src={"img/articles/" + image}/>
-              <div className="w-full md:w-3/5 md:flex items-center">
-                  <div className="space-y-1 md:space-y-3 px-6 py-4">
-                      <p className="text-sm text-highlightColor-400 uppercase">{category}</p>
-                      <p className="text-2xl md:text-2.5xl font-semibold font-medium text-neutral-800 leading-8 md:line-clamp-2 hyphens-auto">{title}</p>
-                      <p className="text-base leading-5 line-clamp-4 md:line-clamp-3">{summary}</p>
-                  </div>
-              </div>
-             </div>
-          ))}
-      </div>
-    );    
+            </div>
+          </div>
+        ))}
+    </div>
+  );
 }
 
 
@@ -77,56 +77,56 @@ function TravelArticles(props) {
   const [error, setError] = useState(null);
 
   useEffect(() => {
-      const getData = async () => {
-        try {
-          const response = await fetch(
-              'https://damp-atoll-27311.herokuapp.com/api/articles/getArticleSummaryByCategory?category=travel'
+    const getData = async () => {
+      try {
+        const response = await fetch(
+          'https://damp-atoll-27311.herokuapp.com/api/articles/getArticleSummaryByCategory?category=travel'
+        );
+        if (!response.ok) {
+          throw new Error(
+            `This is an HTTP error: The status is ${response.status}`
           );
-          if (!response.ok) {
-            throw new Error(
-              `This is an HTTP error: The status is ${response.status}`
-            );
-          }
-          let actualData = await response.json();
-          setData(actualData);
-          console.log(actualData)
-          setError(null);
-        } catch(err) {
-          setError(err.message);
-          setData(null);
-        } finally {
-          setLoading(false);
-        }  
+        }
+        let actualData = await response.json();
+        setData(actualData);
+        console.log(actualData)
+        setError(null);
+      } catch (err) {
+        setError(err.message);
+        setData(null);
+      } finally {
+        setLoading(false);
       }
-      getData()
+    }
+    getData()
 
-    }, [])
+  }, [])
 
-    return (
-      
-      <div className="space-y-7">
-           {loading && 
-              <div className="space-y-7">
-                  <div className="bg-white rounded-lg flex h-44 drop-shadow-md cursor-pointer hover"></div>
-                  <div className="bg-white rounded-lg flex h-44 drop-shadow-md cursor-pointer hover"></div>
-                  <div className="bg-white rounded-lg flex h-44 drop-shadow-md cursor-pointer hover"></div>
+  return (
+
+    <div className="space-y-7">
+      {loading &&
+        <div className="space-y-7">
+          <div className="bg-white rounded-lg flex h-44 drop-shadow-md cursor-pointer hover"></div>
+          <div className="bg-white rounded-lg flex h-44 drop-shadow-md cursor-pointer hover"></div>
+          <div className="bg-white rounded-lg flex h-44 drop-shadow-md cursor-pointer hover"></div>
+        </div>
+      }
+      {data &&
+        data.reverse().map(({ image, title, summary, category, link }) => (
+          <div key={link} className="bg-white rounded-lg flex drop-shadow-md cursor-pointer hover h-fit" onClick={() => window.location.href = ('articles/' + link)}>
+            <img className="hidden md:block md:w-2/5 object-cover rounded-l-lg" src={"img/articles/" + image} />
+            <div className="w-full md:w-3/5 md:flex items-center">
+              <div className="space-y-1 md:space-y-3 px-6 py-4">
+                <p className="text-sm text-highlightColor-400 uppercase">{category}</p>
+                <p className="text-2xl md:text-2.5xl font-semibold font-medium text-neutral-800 leading-8 md:line-clamp-2 hyphens-auto">{title}</p>
+                <p className="text-base leading-5 line-clamp-4 md:line-clamp-3">{summary}</p>
               </div>
-           }
-          {data &&
-          data.reverse().map(({image, title, summary, category, link}) => (
-              <div key={link} className="bg-white rounded-lg flex drop-shadow-md cursor-pointer hover h-fit" onClick={() => window.location.href= ('articles/' + link)}>
-              <img className="hidden md:block md:w-2/5 object-cover rounded-l-lg" src={"img/articles/" + image}/>
-              <div className="w-full md:w-3/5 md:flex items-center">
-                  <div className="space-y-1 md:space-y-3 px-6 py-4">
-                      <p className="text-sm text-highlightColor-400 uppercase">{category}</p>
-                      <p className="text-2xl md:text-2.5xl font-semibold font-medium text-neutral-800 leading-8 md:line-clamp-2 hyphens-auto">{title}</p>
-                      <p className="text-base leading-5 line-clamp-4 md:line-clamp-3">{summary}</p>
-                  </div>
-              </div>
-             </div>
-          ))}
-      </div>
-    );    
+            </div>
+          </div>
+        ))}
+    </div>
+  );
 }
 
 const domContainer2 = document.querySelector('#travelArticlesGenerated');
--- public/fetchAPI_articles.js
+++ public/fetchAPI_articles.js
@@ -6,54 +6,54 @@ const e = React.createElement;
 
 function AllArticles(props) {
 
-    const [article, setArticle] = useState();
-    const [data, setData] = useState(null);
-    const [loading, setLoading] = useState(true);
-    const [error, setError] = useState(null);
+  const [article, setArticle] = useState();
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
 
-    useEffect(() => {
-        const getData = async () => {
-          try {
-            const response = await fetch(
-                'https://damp-atoll-27311.herokuapp.com/api/articles/getAllArticlesSummary'
-            );
-            if (!response.ok) {
-              throw new Error(
-                `This is an HTTP error: The status is ${response.status}`
-              );
-            }
-            let actualData = await response.json();
-            setData(actualData);
-            console.log(actualData)
-            setError(null);
-          } catch(err) {
-            setError(err.message);
-            setData(null);
-          } finally {
-            setLoading(false);
-          }  
+  useEffect(() => {
+    const getData = async () => {
+      try {
+        const response = await fetch(
+          'https://damp-atoll-27311.herokuapp.com/api/articles/getAllArticlesSummary'
+        );
+        if (!response.ok) {
+          throw new Error(
+            `This is an HTTP error: The status is ${response.status}`
+          );
         }
-        getData()
-  
-      }, [])
-      return (
-        
-        <div class="grid md:grid-cols-2 xl:grid-cols-3 sd:grid-cols-1 gap-x-12 gap-y-12 h-full">
-             {loading &&
-                <div class=" grid space-y-7">
-                    <p>Loading...</p>
-                </div>
-             }
-            {data &&
-            data.reverse().map(({image, title, summary, category, link}) => (
-              <div key={link} class="hover cursor-pointer" id="article" onClick={() => window.location.href= ('articles/' + link)}>
-              <img src={"../img/articles/" + image} alt="" class="rounded-lg mb-4 drop-shadow-md"/>
-              <h1 class="text-3xl mb-1 line-clamp-2">{title}</h1>
-              <p class="text-lg line-clamp-2">{summary}</p>
-              </div>
-            ))}
+        let actualData = await response.json();
+        setData(actualData);
+        console.log(actualData)
+        setError(null);
+      } catch (err) {
+        setError(err.message);
+        setData(null);
+      } finally {
+        setLoading(false);
+      }
+    }
+    getData()
+
+  }, [])
+  return (
+
+    <div class="grid md:grid-cols-2 xl:grid-cols-3 sd:grid-cols-1 gap-x-12 gap-y-12 h-full">
+      {loading &&
+        <div class=" grid space-y-7">
+          <p>Loading...</p>
         </div>
-      );
+      }
+      {data &&
+        data.reverse().map(({ image, title, summary, category, link }) => (
+          <div key={link} class="hover cursor-pointer" id="article" onClick={() => window.location.href = ('articles/' + link)}>
+            <img src={"../img/articles/" + image} alt="" class="rounded-lg mb-4 drop-shadow-md" />
+            <h1 class="text-3xl mb-1 line-clamp-2">{title}</h1>
+            <p class="text-lg line-clamp-2">{summary}</p>
+          </div>
+        ))}
+    </div>
+  );
 }
 
 
